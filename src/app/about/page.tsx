cd4ed26e4923e363fb77b651cc1ffb0ffa7f2d79
Mike Guerrero <mikxvii@berkeley.edu>
2025-08-29 15:04:29 -0700

    updated lship section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,7 +11,7 @@ export default function About() {
       <AboutUs />
       <AboutCarousel />
       <Carousel />
-      {/* <Exec /> */}
+      <Exec />
       {/* <ProjectManagers /> */}
       {/* <Members /> */}
     </div>
--- src/app/constants/exec.ts
+++ src/app/constants/exec.ts
@@ -2,13 +2,48 @@ import { ExecMember } from '../types/members'
 
 export const execMembers: ExecMember[] = [
   {
+    name: "Mike Guerrero",
+    title: "President",
+    image: "/exec/mike.jpeg"
+  },
+  {
+    name: "Bonnie Wang",
+    title: "VP of Operations",
+    image: "/exec/bonnie.png"
+  },
+  {
+    name: "Pavithra Arun Anand",
+    title: "VP of Operations",
+    image: "/exec/pavithra.jpg"
+  },
+  {
+    name: "Ria Tao",
+    title: "VP of Internal",
+    image: "/exec/ria.jpg"
+  },
+  {
+    name: "Aarav Desai",
+    title: "VP of Internal",
+    image: "/exec/aarav.jpg"
+  },
+  {
+    name: "Daniel Gil",
+    title: "VP of Projects",
+    image: "/exec/daniel.jpg"
+  },
+  {
     name: "Sodbayar Ganbat",
     title: "VP of Projects",
     image: "/exec/sod.jpeg"
   },
+   {
+    name: "Riana Mistry",
+    title: "VP of Education",
+    image: "/exec/riana.png"
+  },
   {
-    name: "Mike Guerrero",
+    name: "Tina Ye",
     title: "VP of Marketing",
-    image: "/exec/mike.jpeg"
+    image: "/exec/tina.jpg"
   }
 ]
